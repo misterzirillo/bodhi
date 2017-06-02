e04c170addf10881532c6753291d17ccfdda8f47
Matt Cirillo <misterzirillo@gmail.com>
2017-06-02 09:45:17 -0600

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
-var path = require('path');
-var babelRelayPlugin = path.join(__dirname, 'src/main/js/utility/babelRelayPlugin');
+let path = require('path');
+let babelRelayPlugin = path.join(__dirname, 'src/main/js/utility/babelRelayPlugin');
 
 module.exports = {
     entry: {
         index: './src/main/js/index.js'
     },
     output: {
-        path: './grails-app/assets/javascripts',
+        path: path.join(__dirname, './grails-app/assets/javascripts'),
         publicPath: '/assets/',
         filename: 'wp-bundle.js'
     },
 	module: {
-		loaders: [
+		rules: [
 			{
 				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
 				include: path.join(__dirname, 'src/main/js'),
-				loader: 'babel-loader',
-				query: {
-					plugins: [babelRelayPlugin, 'transform-class-properties'],
-					presets: ['react', 'es2015', 'stage-0']
-				}
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env', 'react'],
+						plugins: [babelRelayPlugin, 'transform-class-properties']
+					}
+				},
 			}
 		]
 	}
